850e54f7004ff9f75b8be6e68e72493fbaff4a44
Ty Janoski <tyfolino@gmail.com>
2024-02-03 13:24:01 -0500

    Created a script to download WoFS simulations

---
+++ download-wof.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for VAR in {01..18}
+do
+wget "https://data.nssl.noaa.gov/thredds/fileServer/FRDD/WoFS/Forecast/2021/\
+20210901/2000/ENS_MEM_${VAR}/wrfout_d01_2021-09-01_20:00:00"
+done
