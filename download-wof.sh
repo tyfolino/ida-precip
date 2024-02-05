41c27edcae259fde0101eadbe1fde921b43e4701
Ty Janoski <tyfolino@gmail.com>
2024-02-05 17:09:37 -0500

    made first plot

--- download-wof.sh
+++ download-wof.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-for VAR in {01..18}
+for ENS in {01..18}
 do
-wget -P /mnt/drive2/wof-runs "https://data.nssl.noaa.gov/thredds/fileServer/FRDD/WoFS/Forecast/2021/\
-20210901/2000/ENS_MEM_${VAR}/wrfout_d01_2021-09-01_20:00:00"
+for DYHR in "01_20" "01_21" "01_22" "01_23" "02_00" "02_01"
+do
+for MIN in {00..55..5}
+do
+wget -P /mnt/drive2/wof_runs "https://data.nssl.noaa.gov/thredds/fileServer/FRDD/WoFS/\
+Forecast/2021/20210901/2000/ENS_MEM_${ENS}/wrfwof_d01_2021-09-${DYHR}:${MIN}:00"
+done
+done
+wget -P /mnt/drive2/wof_runs "https://data.nssl.noaa.gov/thredds/fileServer/FRDD/WoFS/\
+Forecast/2021/20210901/2000/ENS_MEM_${ENS}/wrfwof_d01_2021-09-02_02:00:00"
 done
