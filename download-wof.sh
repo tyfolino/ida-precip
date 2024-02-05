8faf6757dfa852f69e54ee0103aabd51ff1f296d
Ty Janoski <tyfolino@gmail.com>
2024-02-05 17:25:29 -0500

    fixed download-wof script

--- download-wof.sh
+++ download-wof.sh
@@ -6,10 +6,12 @@ for DYHR in "01_20" "01_21" "01_22" "01_23" "02_00" "02_01"
 do
 for MIN in {00..55..5}
 do
-wget -P /mnt/drive2/wof_runs "https://data.nssl.noaa.gov/thredds/fileServer/FRDD/WoFS/\
+wget -O "wrfwof_d01_2021-09-${DYHR}:${MIN}:00.${ENS}" \
+"https://data.nssl.noaa.gov/thredds/fileServer/FRDD/WoFS/\
 Forecast/2021/20210901/2000/ENS_MEM_${ENS}/wrfwof_d01_2021-09-${DYHR}:${MIN}:00"
 done
 done
-wget -P /mnt/drive2/wof_runs "https://data.nssl.noaa.gov/thredds/fileServer/FRDD/WoFS/\
+wget -O "wrfwof_d01_2021-09-02_02:00:00.${ENS}" \
+"https://data.nssl.noaa.gov/thredds/fileServer/FRDD/WoFS/\
 Forecast/2021/20210901/2000/ENS_MEM_${ENS}/wrfwof_d01_2021-09-02_02:00:00"
 done
