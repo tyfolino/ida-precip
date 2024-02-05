dd5e9188e0b12a6b0290c64f23447582a3cd66bc
Ty Janoski <tyfolino@gmail.com>
2024-02-05 12:28:48 -0500

    "updated download script"

--- download-wof.sh
+++ download-wof.sh
@@ -2,6 +2,6 @@
 
 for VAR in {01..18}
 do
-wget "https://data.nssl.noaa.gov/thredds/fileServer/FRDD/WoFS/Forecast/2021/\
+wget -P /mnt/drive2/wof-runs "https://data.nssl.noaa.gov/thredds/fileServer/FRDD/WoFS/Forecast/2021/\
 20210901/2000/ENS_MEM_${VAR}/wrfout_d01_2021-09-01_20:00:00"
 done
