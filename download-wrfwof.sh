ac4fca39398ccd153d9c94b042ce9cc7fed40d37
Ty Janoski <tyfolino@gmail.com>
2024-02-16 15:54:34 -0500

    Added probability of exceedance plots

--- download-wrfwof.sh
+++ download-wrfwof.sh
@@ -3,16 +3,16 @@
 for ENS in {01..18}
 do
 # for DYHR in "01_20" "01_21" "01_22" "01_23" "02_00" "02_01"
-for DYHR in "01_23" "02_00" "02_01" "02_02" "02_03" "02_04"
+for DYHR in "02_00" "02_01" "02_02" "02_03" "02_04" "02_05"
 do
 for MIN in {00..55..5}
 do
 wget -O "wrfwof_d01_2021-09-${DYHR}:${MIN}:00.${ENS}" \
 "https://data.nssl.noaa.gov/thredds/fileServer/FRDD/WoFS/\
-Forecast/2021/20210901/2300/ENS_MEM_${ENS}/wrfwof_d01_2021-09-${DYHR}:${MIN}:00"
+Forecast/2021/20210901/0000/ENS_MEM_${ENS}/wrfwof_d01_2021-09-${DYHR}:${MIN}:00"
 done
 done
-wget -O "wrfwof_d01_2021-09-02_05:00:00.${ENS}" \
+wget -O "wrfwof_d01_2021-09-02_06:00:00.${ENS}" \
 "https://data.nssl.noaa.gov/thredds/fileServer/FRDD/WoFS/\
-Forecast/2021/20210901/2300/ENS_MEM_${ENS}/wrfwof_d01_2021-09-02_02:00:00"
+Forecast/2021/20210901/0000/ENS_MEM_${ENS}/wrfwof_d01_2021-09-02_06:00:00"
 done
