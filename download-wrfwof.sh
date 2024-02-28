46dc43e5f025d97ae9a8e584632047210d6f7f36
Ty Janoski <tyfolino@gmail.com>
2024-02-28 18:23:16 -0500

    Started precip histograms notebook

--- download-wrfwof.sh
+++ download-wrfwof.sh
@@ -2,17 +2,17 @@
 
 for ENS in {01..18}
 do
-for DYHR in "01_22" "01_23" "02_00" "02_01" "02_02" "02_03"
+for DYHR in "01_21" "01_22" "01_23" "02_00" "02_01" "02_02"
 # for DYHR in "02_00" "02_01" "02_02" "02_03" "02_04" "02_05"
 do
 for MIN in {00..55..5}
 do
 wget -O "wrfwof_d01_2021-09-${DYHR}:${MIN}:00.${ENS}" \
 "https://data.nssl.noaa.gov/thredds/fileServer/FRDD/WoFS/\
-Forecast/2021/20210901/2200/ENS_MEM_${ENS}/wrfwof_d01_2021-09-${DYHR}:${MIN}:00"
+Forecast/2021/20210901/2100/ENS_MEM_${ENS}/wrfwof_d01_2021-09-${DYHR}:${MIN}:00"
 done
 done
-wget -O "wrfwof_d01_2021-09-02_04:00:00.${ENS}" \
+wget -O "wrfwof_d01_2021-09-02_03:00:00.${ENS}" \
 "https://data.nssl.noaa.gov/thredds/fileServer/FRDD/WoFS/\
-Forecast/2021/20210901/2200/ENS_MEM_${ENS}/wrfwof_d01_2021-09-02_04:00:00"
+Forecast/2021/20210901/2100/ENS_MEM_${ENS}/wrfwof_d01_2021-09-02_03:00:00"
 done
---
+++ extract-var.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Get user input
+echo "Input initialization time in the form ##Z"
+read init
+echo "Input variable name."
+read var
+
+# Specify the directory
+directory="/mnt/drive2/wof-runs/$init"
+
+# Make a new directory
+mkdir "$directory/$var"
+
+# iterate through each file in the directory
+for file in $directory/*
+do
+    if [ -f "$file" ]; then
+        filename=$(basename "$file")
+        # echo "$directory/$var/$filename"
+        ncks -v $var $file "$directory/$var/$filename"
+    fi
+done
