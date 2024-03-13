c9ab54ffe4dc41340e367c9e67087094c2845a1c
Ty Janoski <tyfolino@gmail.com>
2024-03-13 16:29:25 -0400

    Reorganized file structure, began a utils module

---
+++ utils/utils.py
@@ -0,0 +1,70 @@
+# import statements
+import xarray as xr
+import numpy as np
+import argparse
+
+def make_z(filename: str, path: str) -> None:
+    # Read in necessary variables
+    ph = xr.open_dataarray(path+"/PH/"+filename,decode_times=False)
+    phb = xr.open_dataarray(path+"/PHB/"+filename,decode_times=False)
+    p = xr.open_dataarray(path+"/P/"+filename,decode_times=False)
+
+    # Calculate height on the staggered grid, then move to centered point
+    z_stag = (ph + phb)/9.81
+    z = 0.5 * (
+        z_stag.isel(bottom_top_stag=slice(None,-1)) + 
+        z_stag.isel(bottom_top_stag=slice(1,None))
+    )
+
+    # Fix dims & coordinates
+    z = z.assign_coords(coords=p.coords).reindex_like(p).rename("z")
+    z = z.rename({"bottom_top_stag":"bottom_top"})
+
+    z = z.assign_attrs(
+        {
+            "units":"m",
+            "description":"heights on native vertical grid"
+        }
+    )
+
+    # Save out height as netCDF
+    z.to_netcdf(path+"/z/"+filename)
+
+def make_mslp(filename: str, path: str) -> None:
+    # Read in necessary variables
+    t2 = xr.open_dataarray(path+"/T2/"+filename,decode_times=False)
+    hgt = xr.open_dataarray(path+"/HGT/"+filename,decode_times=False)
+    psfc = xr.open_dataarray(path+"/PSFC/"+filename,decode_times=False)
+
+    # Convert the height to kilometers
+    hgt_km = hgt/1000
+    
+    # Assume global average tropospheric lapse rate
+    stemps = t2+(6.5*hgt_km)
+
+    # Calculate mean sea level pressure
+    mslp = psfc*np.exp(9.81/(287*stemps)*hgt)*0.01 + (6.7*hgt_km)
+    mslp = mslp.rename("mslp") * 100
+    mslp.attrs["units"] = 'Pa'
+
+    # Save out MSLP as netCDF
+    mslp.to_netcdf(path+"/mslp/"+filename)
+
+def put_z_on_plevs(filename: str, path: str) -> None:
+    pass # Start here tomorrow
+
+def main():
+    parser = argparse.ArgumentParser(description='Calculate variables from WoFS.')
+    parser.add_argument('va', type=str, help='Name of the function to call')
+    parser.add_argument("filename", help="The name of the file")
+    parser.add_argument("path", help="The path of the desired WoFS initialization.")
+    args = parser.parse_args()
+
+    if args.function == 'make_z':
+        make_z(args.filename, args.path)
+    elif args.function == "make_mslp":
+        make_mslp(args.filename, args.path)
+    # Add more conditions for other functions as needed
+
+if __name__ == "__main__":
+    main()
