665237f17c4a51e80d69e63b8808180e4ed40ab2
Ty Janoski <tyfolino@gmail.com>
2024-03-15 17:56:19 -0400

    Began filtering

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,7 @@
 import xarray as xr
 import numpy as np
 import argparse
+from xgcm import Grid
 
 def make_z(filename: str, path: str) -> None:
     # Read in necessary variables
@@ -50,20 +51,44 @@ def make_mslp(filename: str, path: str) -> None:
     # Save out MSLP as netCDF
     mslp.to_netcdf(path+"/mslp/"+filename)
 
-def put_z_on_plevs(filename: str, path: str) -> None:
-    pass # Start here tomorrow
+def put_var_on_plevs(filename: str, path: str, varname: str) -> None:
+    # Read in variables
+    p = xr.open_dataarray(path+"/P/"+filename,decode_times=False)
+    pb = xr.open_dataarray(path+"/PB/"+filename,decode_times=False)
+    var = xr.open_dataarray(path+"/"+varname+"/"+filename,decode_times=False)
+
+    # Create pressure field
+    pres = (p + pb).rename('pres')
+    ds_pres = xr.merge([pres,var])
+
+    # Create grid and pressure levels
+    grid = Grid(ds_pres, coords={"Z":{"center":"bottom_top"}}, periodic=False)
+    levels = np.array([85000, 70000, 50000, 30000, 20000])
+
+    # Regrid
+    var_pres = grid.transform(ds_pres.varname,"Z",levels,target_data=ds_pres.pres,
+                              method="log")
+    var_pres = var_pres.rename(varname)
+    
+    # Save out variable
+    var_pres.to_netcdf(path+"/"+varname+"_plev/"+filename)
 
 def main():
     parser = argparse.ArgumentParser(description='Calculate variables from WoFS.')
-    parser.add_argument('va', type=str, help='Name of the function to call')
+    parser.add_argument('function', type=str, help='Name of the function to call')
     parser.add_argument("filename", help="The name of the file")
     parser.add_argument("path", help="The path of the desired WoFS initialization.")
+    parser.add_argument(
+        "--varname",nargs="?",const="z",help="The name of the variable to put on\
+            plevs.")
     args = parser.parse_args()
 
     if args.function == 'make_z':
         make_z(args.filename, args.path)
     elif args.function == "make_mslp":
         make_mslp(args.filename, args.path)
+    elif args.function == "put_var_on_plevs":
+        put_var_on_plevs(args.filename,args.path,args.varname)
     # Add more conditions for other functions as needed
 
 if __name__ == "__main__":
