519fd28e342fb742535950f6f4b1547ac712c3ed
Ty Janoski <tyfolino@gmail.com>
2024-05-08 14:58:26 -0400

    Edited extracting script to put relative vorticity and wind components

--- extracting_script.sh
+++ extracting_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for init in "20Z" "01Z"
+for init in "20Z" "21Z" "22Z" "23Z"
 do
-    for var in "P"
+    for var in "REL_VORT" "U" "V"
     do
         bash /home/janoski/ida-scripts/extract-var.sh $init $var
     done
